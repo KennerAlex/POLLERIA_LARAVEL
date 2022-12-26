29f2cf4b4e582b20845d405697601086ebd459cc
Ayrton Soto <ayrtonxd.123@gmail.com>
2022-12-26 13:49:22 -0500

    Haciendo correcciones

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use \App\Models\tipousuario;
+use \App\Models\tipoUsuario;
 use \App\Models\Trabajador;
 use \Illuminate\Http\Request;
 use \Illuminate\Support\Facades\Auth;
